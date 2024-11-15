e856346a4e5f31128dd1ededc2e52f27f53fe349
Danielle Donnelly <145680391+danielledonnelly@users.noreply.github.com>
2024-11-14 20:45:11 -0330

    ya

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://danielledonnelly.github.io',
   output: 'static',
-  // base: '/2025-portfolio/',
+  base: '/2025-portfolio/',
 });
